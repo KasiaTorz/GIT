aeda2a0b3bc4c748550aead50fa9d051e5170a8b
KasiaTorz <ktorz@wp.pl>
2018-04-20 16:01:27 +0200

    event onclick

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*event click to add task in input and function callback*/
+
+document.getElementById('add').addEventListener('click',function(){
+    var value= document.getElementById('item').value;
+    if (value) {
+        console.log('ale jaja');
+    }
+
+});
