68f73b0d9cf198226f6376e7fbdd40edd5bdda67
KasiaTorz <ktorz@wp.pl>
2018-05-12 18:11:10 +0200

    add class to btn

--- js/task-list.js
+++ js/task-list.js
@@ -4,3 +4,13 @@ tasks.forEach(function(title){
     addNewTask(title);
 });
 }
+//function to delete//
+function deleteTask(){
+
+};
+
+//function to complete//
+function toggleTaskComplete(task){ 
+    task.classList.toggle('btn-success');
+}
+    
