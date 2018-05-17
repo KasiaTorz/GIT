bbe0eed10fae6e78b1c63a80343c35ad466dff39
KasiaTorz <ktorz@wp.pl>
2018-05-17 15:07:50 +0200

    make statement

--- js/main.js
+++ js/main.js
@@ -138,5 +138,8 @@ document.addEventListener('DOMContentLoaded',function(){
 function modify_qty(val){
   var qty= document.getElementById('qty').value;
   var new_qty= parseInt(qty,10)+val;
+  if (new_qty<0){
+      new_qty=0;
+  }
   
 }
